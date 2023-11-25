833072169050477e9944c943bdbf512ca46088f8
ndatn <ndaten0@gmail.com>
2023-11-26 02:42:47 +0700

    add app

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// App.js
+import React, { useState } from 'react';
+import AddProduct from './app/AddProduct';
+import SearchProducts from './app/SearchProducts';
 
 function App() {
   const [products, setProducts] = useState([]);
-  const [newProduct, setNewProduct] = useState('');
-  const [searchTerm, setSearchTerm] = useState('');
 
-  useEffect(() => {
-    axios.get('/api/products').then(response => {
-      setProducts(response.data);
-    });
-  }, []);
-
-  const addProduct = () => {
-    axios.post('/api/products', { name: newProduct }).then(() => {
-      setProducts([...products, newProduct]);
-      setNewProduct('');
-    });
+  const addProduct = productName => {
+    setProducts([...products, productName]);
   };
 
-  const filteredProducts = products.filter(product =>
-    product.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const searchProducts = term => {
+    const result = products.filter(product =>
+      product.toLowerCase().includes(term.toLowerCase())
+    );
+    console.log('Search result:', result);
+  };
 
   return (
     <div>
-      <h1>Cloud App</h1>
-      <input
-        type="text"
-        value={newProduct}
-        onChange={e => setNewProduct(e.target.value)}
-      />
-      <button onClick={addProduct}>Add Product</button>
-      <input
-        type="text"
-        placeholder="Search Products"
-        value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)}
-      />
-      <ul>
-        {filteredProducts.map((product, index) => (
-          <li key={index}>{product}</li>
-        ))}
-      </ul>
+      <h1>Cloud Application</h1>
+      <AddProduct onAddProduct={addProduct} />
+      <SearchProducts onSearchProducts={searchProducts} />
     </div>
   );
 }
