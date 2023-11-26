cc6cf4ffac94c86b791178cf07ecc6ebc2c548a5
ndatn <ndaten0@gmail.com>
2023-11-26 16:59:43 +0700

    new

--- src/server/ProductService.js
+++ src/server/ProductService.js
@@ -1,7 +1,7 @@
 // ProductService.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://0.0.0.0:3001/api';
 
 const ProductService = {
   getProducts: async () => {
